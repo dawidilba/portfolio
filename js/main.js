87c9dcf06d81a9821aa4b0a7e652da26e4bf9610
dawidilba <dawidilba@gmail.com>
2020-08-16 00:14:07 +0200

    Skills animation

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// $(document).ready(function () {
-//     $(document).click(function (event) {
-//         var clickover = $(event.target);
-//         var _opened = $(".navbar-collapse").hasClass("show");
-//         if (_opened === true && !clickover.hasClass("navbar-toggler")) {
-//             $(".navbar-toggler").click();
-//         }
-//     });
-// }); KOD JQuery
 function main(){
     prepareDOMElements();
     prepareDOMEvents();
