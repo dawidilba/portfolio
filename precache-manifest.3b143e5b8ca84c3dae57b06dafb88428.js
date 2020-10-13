6a3e01818d4558b9527cecbe52be5dfaf581bd6e
dawidilba <dawidilba@gmail.com>
2020-10-13 19:08:07 +0200

    Updates

---
+++ precache-manifest.3b143e5b8ca84c3dae57b06dafb88428.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b131926cba83762aa57fc2dfeb07dc2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "72f23df75bf4360dc869",
+    "url": "/portfolio/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "d5148454394e440ee7ca",
+    "url": "/portfolio/static/css/main.ccb2046d.chunk.css"
+  },
+  {
+    "revision": "72f23df75bf4360dc869",
+    "url": "/portfolio/static/js/2.6871b1e4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.6871b1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5148454394e440ee7ca",
+    "url": "/portfolio/static/js/main.aecb7c91.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a174660bdd1e83c2e1d40bf5397a151b",
+    "url": "/portfolio/static/media/b_1280.a174660b.jpg"
+  },
+  {
+    "revision": "c216431a93e752f9c0ae5c72fd325a3d",
+    "url": "/portfolio/static/media/b_1920.c216431a.jpg"
+  },
+  {
+    "revision": "39488bf3cc2638a1244bd5371d2f4c79",
+    "url": "/portfolio/static/media/b_640.39488bf3.jpg"
+  },
+  {
+    "revision": "20a2cf2d53803200fdb149c11b12043b",
+    "url": "/portfolio/static/media/contact_1280.20a2cf2d.jpg"
+  },
+  {
+    "revision": "023ac5665452282bba24eb5a82732a02",
+    "url": "/portfolio/static/media/contact_1920.023ac566.jpg"
+  },
+  {
+    "revision": "5926274cf3d35979ffe3bdb02c8da35f",
+    "url": "/portfolio/static/media/contact_640.5926274c.jpg"
+  }
+]);
